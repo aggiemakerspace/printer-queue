f03452b787dab78e0919f563b5116cad2d5b58a2
Chris Cannon <chris.c.canon@gmail.com>
2017-11-03 11:26:44 -0400

    Encapsulate PrinterQueue arrayList

--- PrinterQueue/src/printerqueue/PrinterQueue.java
+++ PrinterQueue/src/printerqueue/PrinterQueue.java
@@ -5,10 +5,32 @@
  */
 package printerqueue;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author CCannon
  */
 public class PrinterQueue {
+    private ArrayList<PrintJob> queue;
     
+    public int getNumPrintJobs() {
+        return queue.size();
+    }
+    
+    public void addPrintJob(PrintJob job) {
+        queue.add(job);
+    }
+    
+    public void setPrintJob(int index, PrintJob job) {
+        queue.set(index, job);
+    }
+    
+    public PrintJob getPrintJob(int index) {
+        return queue.get(index);
+    }
+    
+    public PrintJob removePrintJob(int index) {
+        return queue.remove(index);
+    }
 }
