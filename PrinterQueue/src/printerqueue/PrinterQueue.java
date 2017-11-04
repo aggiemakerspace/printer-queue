e77e9a61f90ecb67d03c05dada5e7e6b7fb4c89b
Chris Cannon <chris.c.canon@gmail.com>
2017-11-04 15:00:41 -0400

    Add waiting and completed arrayLists to PrinterQueue

--- PrinterQueue/src/printerqueue/PrinterQueue.java
+++ PrinterQueue/src/printerqueue/PrinterQueue.java
@@ -13,9 +13,17 @@ import java.util.ArrayList;
  */
 public class PrinterQueue {
     private ArrayList<PrintJob> queue;
+    private ArrayList<PrintJob> waitingForPickup;
+    private ArrayList<PrintJob> completed;
+    
+    private final String queueConfigFileName = "queueConfig.txt";
+    private final String waitingForPickupConfigFileName = "waitingForPickupConfig.txt";
+    private final String completedConfigFileName = "completedConfig.txt";
     
     public PrinterQueue() {
         queue = new ArrayList<PrintJob>();
+        waitingForPickup = new ArrayList<PrintJob>();
+        completed = new ArrayList<PrintJob>();
     }
     
     public int getNumPrintJobs() {
@@ -36,5 +44,45 @@ public class PrinterQueue {
     
     public PrintJob removePrintJob(int index) {
         return queue.remove(index);
+    }
+    
+    public int getNumPrintJobsWaitingForPickup() {
+        return waitingForPickup.size();
+    }
+    
+    public void addPrintJobWaitingForPickup(PrintJob job) {
+        waitingForPickup.add(job);
+    }
+    
+    public void setPrintJobWaitingForPickup(int index, PrintJob job) {
+        waitingForPickup.set(index, job);
+    }
+    
+    public PrintJob getPrintJobWaitingForPickup(int index) {
+        return waitingForPickup.get(index);
+    }
+    
+    public PrintJob removePrintJobWaitingForPickup(int index) {
+        return waitingForPickup.remove(index);
+    }
+    
+    public int getNumCompletedPrintJobs() {
+        return completed.size();
+    }
+    
+    public void addCompletedPrintJob(PrintJob job) {
+        completed.add(job);
+    }
+    
+    public void setCompletedPrintJob(int index, PrintJob job) {
+        completed.set(index, job);
+    }
+    
+    public PrintJob getCompletedPrintJob(int index) {
+        return completed.get(index);
+    }
+    
+    public PrintJob removeCompletedPrintJob(int index) {
+        return completed.remove(index);
     }
 }
