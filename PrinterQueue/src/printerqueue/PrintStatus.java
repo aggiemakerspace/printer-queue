f95393c883fa5f38c4005c7061ffc9ff0262dba6
Chris Cannon <chris.c.cannon@gmail.com>
2017-11-08 21:03:09 -0500

    Add printStatus enum

---
+++ PrinterQueue/src/printerqueue/PrintStatus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printerqueue;
+
+/**
+ *
+ * @author CCannon
+ */
+public enum PrintStatus {
+    READY_TO_PRINT, READY_FOR_PICKUP, COMPLETED;
+}
