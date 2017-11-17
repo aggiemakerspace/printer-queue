258f773d8fd3a223955c2a36b4ca5be04fb084e8
Chris Cannon <chris.c.cannon@gmail.com>
2017-11-16 23:13:30 -0500

    Complete basic styling for 0.1

--- PrinterQueue/src/printerqueueviewcontroller/PrinterQueueViewController.java
+++ PrinterQueue/src/printerqueueviewcontroller/PrinterQueueViewController.java
@@ -27,6 +27,7 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -79,8 +80,12 @@ public class PrinterQueueViewController extends Application {
         BorderPane root = new BorderPane();
 
         Label topLabel = new Label("3-D Printer Queue");
+        topLabel.setStyle("-fx-font-size: 48;");
+        topLabel.setAlignment(Pos.CENTER);
         HBox contentPane = new HBox();
         VBox commandPane = new VBox();
+        commandPane.setAlignment(Pos.CENTER);
+        commandPane.setSpacing(10.0);
 
         VBox queuePane = new VBox();
         queueListView = new ListView();
